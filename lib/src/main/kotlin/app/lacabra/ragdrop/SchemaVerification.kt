0312c257688dc34ded53064a44e9dbff0465b604
holasoyender <javier.projectzafire@gmail.com>
2023-06-20 01:50:00 +0200

    fix: remove debug logs

--- lib/src/main/kotlin/app/lacabra/ragdrop/SchemaVerification.kt
+++ lib/src/main/kotlin/app/lacabra/ragdrop/SchemaVerification.kt
@@ -1,6 +1,5 @@
 package app.lacabra.ragdrop
 
-import app.lacabra.ragdrop.types.Number
 import org.json.JSONObject
 import kotlin.reflect.KFunction1
 
@@ -19,20 +18,14 @@ class SchemaVerification {
             val roots = schema.keys()
             for (root in roots) {
 
-                println("Checking root '$root'")
-
                 val rootObject = schema.getJSONObject(root)
-
                 val aliases = try { rootObject.getJSONArray("aliases").toList().map { it.toString() } } catch (e: Exception) { listOf() }
-                println("Aliases: $aliases")
                 val required = try { rootObject.getBoolean("required") } catch (e: Exception) { true }
-                println("Required: $required")
-                val description = try { rootObject.getString("description") } catch (e: Exception) { "" }
-                println("Description: $description")
+                //val description = try { rootObject.getString("description") } catch (e: Exception) { "" }
                 if (aliases.any { it == root })
                     throw BadSchemaException("Root '$root' is an alias for itself")
 
-                val defaultValue = if (!required) {
+                if (!required) {
                     try {
                         rootObject.get("default")
                     } catch (e: Exception) {
@@ -41,21 +34,16 @@ class SchemaVerification {
                 } else {
                     try {
                         rootObject.getString("default")
-                    } catch (e: Exception) {
-                        null
-                    }
+                    } catch (_: Exception) {}
                 }
 
-                println("Default value: $defaultValue")
                 val rawDataType = try {
                     rootObject.getString("type")
                 } catch (e: Exception) {
                     throw BadSchemaException("Type not specified or unknown for root '$root")
                 }
-                println("Raw data type: $rawDataType")
 
                 val rawType = rawDataType.split(" ").getOrNull(0)?.split("[")?.getOrNull(0) ?: throw BadSchemaException("Type '$rawDataType' not found for root '$root'")
-                println("Raw type: $rawType")
 
                 val obj = if (rawType == "map") {
                     try {
@@ -72,29 +60,10 @@ class SchemaVerification {
                 val type = dataType(rawDataType)
                 type.withTypes(types)
 
-                when (rawType) {
-                    "string" -> {
-                        (type as app.lacabra.ragdrop.types.String).let {
-                            println("String min length: ${it.minLength}")
-                            println("String max length: ${it.maxLength}")
-                        }
-                    }
-                    "number" -> {
-                        (type as Number).let {
-                            println("Number min value: ${it.minValue}")
-                            println("Number max value: ${it.maxValue}")
-                        }
-                    }
-                    "array" -> {
-                        println("Array of ${(type as app.lacabra.ragdrop.types.Array).type}")
-                    }
-                }
-
                 if (!type.verify())
                     throw BadSchemaException("Type '$rawType' is invalid for root '$root'")
 
             }
-
             return true
         }
 

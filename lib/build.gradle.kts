cc7eaee162e0e1b87d045f2148cee3912de0ac00
holasoyender <javier.projectzafire@gmail.com>
2023-06-24 00:44:07 +0200

    remove jitpack file

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 
 group = "app.lacabra"
 description = "YAML verification with JSON schemas"
-version = "0.0.2"
+version = "0.0.3"
 
 repositories {
     // Use Maven Central for resolving dependencies.

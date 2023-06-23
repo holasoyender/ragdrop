55112c0871971ddbcd0035da67efd6bea63e0b0d
holasoyender <javier.projectzafire@gmail.com>
2023-06-23 18:15:00 +0200

    impl: Verification algorithm (missing type verification)

--- example/src/main/java/Main.java
+++ example/src/main/java/Main.java
@@ -1,12 +1,16 @@
 import app.lacabra.ragdrop.Schema;
+import app.lacabra.ragdrop.Yaml;
 
 public class Main {
 
     public static void main(String[] args) {
 
         Schema schema = new Schema("classpath:schema.json");
+        Yaml yaml = new Yaml().loadFromPath("example/config.yaml");
 
         System.out.println(schema.verify());
         System.out.println(schema.getErrorMessage());
+
+        System.out.println(schema.validate(yaml));
     }
 }
--- lib/src/main/kotlin/app/lacabra/ragdrop/SchemaVerification.kt
+++ lib/src/main/kotlin/app/lacabra/ragdrop/SchemaVerification.kt
@@ -1,6 +1,7 @@
 package app.lacabra.ragdrop
 
 import app.lacabra.ragdrop.exceptions.BadSchemaException
+import app.lacabra.ragdrop.exceptions.BadYamlException
 import org.json.JSONObject
 import kotlin.reflect.KFunction1
 
@@ -20,8 +21,16 @@ class SchemaVerification {
             for (root in roots) {
 
                 val rootObject = schema.getJSONObject(root)
-                val aliases = try { rootObject.getJSONArray("aliases").toList().map { it.toString() } } catch (e: Exception) { listOf() }
-                val required = try { rootObject.getBoolean("required") } catch (e: Exception) { true }
+                val aliases = try {
+                    rootObject.getJSONArray("aliases").toList().map { it.toString() }
+                } catch (e: Exception) {
+                    listOf()
+                }
+                val required = try {
+                    rootObject.getBoolean("required")
+                } catch (e: Exception) {
+                    true
+                }
                 //val description = try { rootObject.getString("description") } catch (e: Exception) { "" }
                 if (aliases.any { it == root })
                     throw BadSchemaException("Root '$root' is an alias for itself")
@@ -35,7 +44,8 @@ class SchemaVerification {
                 } else {
                     try {
                         rootObject.getString("default")
-                    } catch (_: Exception) {}
+                    } catch (_: Exception) {
+                    }
                 }
 
                 val rawDataType = try {
@@ -44,7 +54,9 @@ class SchemaVerification {
                     throw BadSchemaException("Type not specified or unknown for root '$root")
                 }
 
-                val rawType = rawDataType.split(" ").getOrNull(0)?.split("[")?.getOrNull(0) ?: throw BadSchemaException("Type '$rawDataType' not found for root '$root'")
+                val rawType = rawDataType.split(" ").getOrNull(0)?.split("[")?.getOrNull(0) ?: throw BadSchemaException(
+                    "Type '$rawDataType' not found for root '$root'"
+                )
 
                 val obj = if (rawType == "map") {
                     try {
@@ -54,7 +66,7 @@ class SchemaVerification {
                     }
                 } else null
 
-                if(obj != null)
+                if (obj != null)
                     return verifySchema(obj, types)
 
                 val dataType = types[rawType] ?: throw BadSchemaException("Type '$rawType' not found for root '$root'")
@@ -68,5 +80,134 @@ class SchemaVerification {
             return true
         }
 
+        /**
+         * Verify the given YAML against the given JSON object
+         * @param yaml The YAML to verify
+         * @param json The JSON object to verify against
+         * @param types The types to use
+         * @return Whether the YAML is valid
+         * @throws BadSchemaException If the YAML is invalid
+         */
+        fun verifyYaml(
+            yaml: Map<Any, Any>,
+            json: JSONObject,
+            types: Map<String, KFunction1<String, Type>>,
+            rootName: String = "default"
+        ): Boolean {
+
+            val yamlRoots = yaml.keys
+            val jsonRoots = json.keySet()
+
+
+            /** -----------------
+             *  PHASE 1:
+             *  Check if all required roots are present in the YAML
+             */
+
+            // list of required roots and their aliases
+            val requiredRoots: List<Pair<String, List<String>>> =
+                jsonRoots.filter { json.getJSONObject(it).getBoolean("required") }.toList().let { mp ->
+                    mp.map { root ->
+                        val aliases = json.getJSONObject(root).getJSONArray("aliases").toList().map { it.toString() }
+                        Pair(root, aliases)
+                    }
+                }
+
+            val missingRoots = requiredRoots.filter { (root, aliases) ->
+                !yamlRoots.contains(root) && aliases.none { alias ->
+                    yamlRoots.contains(alias)
+                }
+            }
+
+            if (missingRoots.isNotEmpty())
+                throw BadYamlException("Missing required roots: ${missingRoots.joinToString(", ") { (root, _) -> "'$root'" }} ${if (rootName == "default") "" else "in root '$rootName'"}")
+
+            /** -----------------
+             * PHASE 2:
+             * Compare the types of the roots in the YAML to the types in the JSON schema
+             */
+
+            for (yamlRoot in yamlRoots) {
+
+                // yamlRoot is the root name to check
+                // yaml[yamlRoot] is the value of the root
+
+                /** -----------------
+                 *  PHASE 2.1:
+                 *  Check if the root is in the JSON schema
+                 */
+
+                val schemaObject = if (!jsonRoots.contains(yamlRoot)) {
+                    // The root is not in the JSON schema, so we need to check if it's an alias
+
+                    val obj = jsonRoots.firstOrNull { rt ->
+                        println(rt)
+                        json.getJSONObject(rt).getJSONArray("aliases").toList().map { it.toString().lowercase() }
+                            .contains(yamlRoot.toString().lowercase()) || rt.lowercase() == yamlRoot.toString()
+                            .lowercase()
+                    }
+                        ?: throw BadSchemaException("Root '$yamlRoot' is not in the schema ${if (rootName == "default") "" else "of root '$rootName'"}")
+
+                    json.getJSONObject(obj)
+                } else
+                    json.getJSONObject(yamlRoot.toString())
+
+                /** -----------------
+                 *  PHASE 2.2:
+                 *  At this point we already checked that all required roots are present in the YAML, so we can start verifying the types
+                 *  Get the JSON schema type
+                 */
+
+                // The root is in the JSON schema, now we need to check if the value is valid for the type
+
+                val rawDataType = schemaObject.getString("type")
+                val rawType = rawDataType.split(" ").getOrNull(0)?.split("[")?.getOrNull(0) ?: throw BadSchemaException(
+                    "Type '$rawDataType' not found for root '$yamlRoot' ${if (rootName == "default") "" else "in root '$rootName'"}"
+                )
+                val dataType =
+                    types[rawType]
+                        ?: throw BadSchemaException("Type '$rawType' not found for root '$yamlRoot' ${if (rootName == "default") "" else "in root '$rootName'"}")
+
+                val obj = if (rawType == "map") {
+                    try {
+                        schemaObject.getJSONObject("object")
+                    } catch (e: Exception) {
+                        null
+                    }
+                } else null
+
+                @Suppress("UNCHECKED_CAST")
+                if (obj != null) {
+                    val t = try {
+                        yaml[yamlRoot] as Map<Any, Any>
+                    } catch (e: Exception) {
+                        throw BadYamlException("Value '${yaml[yamlRoot]}' is invalid for root '$yamlRoot': ${e.message} ${if (rootName == "default") "" else "in root '$rootName'"}")
+                    }
+
+                    return verifyYaml(t, obj, types, yamlRoot.toString())
+                }
+
+                val type = dataType(rawDataType)
+                type.withTypes(types)
+
+                /** -----------------
+                 *  PHASE 2.3:
+                 *  Check if the value is valid for the type
+                 */
+
+                val validate =
+                    try {
+                        type.validate(yaml[yamlRoot].toString())
+                    } catch (e: BadYamlException) {
+                        throw BadYamlException("Value '${yaml[yamlRoot]}' is invalid for root '$yamlRoot': ${e.message} ${if (rootName == "default") "" else "in root '$rootName'"}")
+                    }
+
+                if (!validate)
+                    throw BadYamlException("Value '${yaml[yamlRoot]}' is invalid for root '$yamlRoot', expected type '$rawType' ${if (rootName == "default") "" else "in root '$rootName'"}")
+
+            }
+
+            return true
+        }
     }
 }
